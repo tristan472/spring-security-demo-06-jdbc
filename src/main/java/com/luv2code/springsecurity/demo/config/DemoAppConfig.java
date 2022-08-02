f40a89d58111e3e23c71852ca8c23f26dd54362f
tristan472 <65858998+tristan472@users.noreply.github.com>
2022-08-02 19:32:54 +0200

    spring-security-demo-06-jdbc

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -56,16 +56,39 @@ public class DemoAppConfig {
 		try {
 			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
 		} catch (PropertyVetoException e) {
-			// TODO Auto-generated catch block
 			throw new RuntimeException(e);
 		}
 		
 		// log the connexion props
 		
-		// set connexion pool props
+		logger.info(">>jdbc.url = " + env.getProperty("jdbc.url"));
+        logger.info(">>jdbc.user = " + env.getProperty("jdbc.user"));
+		
+		// set database connection pool props
+        
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+        
+        // set connection pool props
+        
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
 		
 		return securityDataSource;
 	}
+	
+	// need a helper method
+	// read environment property and convert to int
+	
+	private int getIntProperty(String propName) {
+        String propVal = env.getProperty(propName);
+        
+        int intPropVal = Integer.parseInt(propVal);
+        return intPropVal;
+    }
 	
 }
 
--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.luv2code.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,17 +15,26 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	// add a reference to the our security data source
+	@Autowired
+	private DataSource securityDataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// use jdbc authentication
+		
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 
 		// add our users for in memory authentication
 		
-		UserBuilder users = User.withDefaultPasswordEncoder();
+//		UserBuilder users = User.withDefaultPasswordEncoder();
+//		
+//		auth.inMemoryAuthentication()
+//			.withUser(users.username("employe").password("test123").roles("EMPLOYEE"))
+//			.withUser(users.username("manager").password("test123").roles("EMPLOYEE", "MANAGER"))
+//			.withUser(users.username("admin").password("test123").roles("EMPLOYEE", "ADMIN"));
 		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("employe").password("test123").roles("EMPLOYEE"))
-			.withUser(users.username("manager").password("test123").roles("EMPLOYEE", "MANAGER"))
-			.withUser(users.username("admin").password("test123").roles("EMPLOYEE", "ADMIN"));
 	}
 
 	@Override
